452ceba721ece3c470747dc8af4cd87938445341
MD HAKIKUL <thehakikul@gmail.com>
2025-08-22 23:07:17 +0530

    Add files via upload

---
+++ Evenandodd.py
@@ -0,0 +1,18 @@
+a=int(input("Enter any number:"))
+
+if(a%2==0):
+    print(a,"is an even number.")
+
+
+else:
+    print(a,"is an odd number.")       
+
+
+
+
+                                  
+ 
+
+
+
+
